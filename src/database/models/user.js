fc828a972770df17664f4a5e9c598f6bfe86b895
fabionicolau <fabionicolaujr@gmail.com>
2022-07-13 17:21:57 -0300

    feat: cria o model User com as propriedades dos usuários

---
+++ src/database/models/user.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const userModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    displayName: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    email: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    password: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    image: {
+      allowNull: true,
+      type: DataTypes.STRING
+    },
+  }, {
+    tableName: 'Users',
+    timestamps: false,
+    underscored: false,
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPosts, {
+      foreignKey: 'userId',
+      as: 'BlogPosts',
+    });
+  }
+
+  return User;
+}
+
+module.exports = userModel;
